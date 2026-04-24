33dcf78144d3f35d746fd3b060e43754245c146d
geodevcode <geodevinstitute@gmail.com>
2026-04-24 07:49:27 +0100

    add Tanstack and Theme providers; update dependencies in package.json and package-lock.json

---
+++ providers/TanstackProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default TanstackProvider;
